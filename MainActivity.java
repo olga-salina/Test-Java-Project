c14f55ce0b0e4ade9f8989ac8cdd537748ab331d
olga <85924352+olga-salina@users.noreply.github.com>
2024-10-22 19:37:49 +0300

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,30 @@
+package com.zac4j.web.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import com.zac4j.web.R;
+import com.zac4j.web.Utils;
+import com.zac4j.web.browser.DialogBrowserLoader;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gotoApproach(View view) {
+        switch (view.getId()) {
+            case R.id.approach_one:
+                startActivity(new Intent(MainActivity.this, PrimaryActivity.class));
+                break;
+            case R.id.approach_two:
+                startActivity(new Intent(MainActivity.this, TertiaryActivity.class));
+                break;
+        }
+    }
+}
